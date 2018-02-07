9c10907d1b6137913c2e7be19ecb034b5d29bff0
Kye <ksolomon@csumb.edu>
2018-02-07 17:47:56 +0000

    editing heroku landing page

---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Heroku Homepage</title>
+    </head>
+    <body>
+        <div>
+            <h1>Labs</h1>
+            <a href="/labs/lab1">Lab 1</a>
+        </div>
+    </body>
+
+</html>
